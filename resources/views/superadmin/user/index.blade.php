bd5cb064217c855c531a874a08d68ac94191ce89
MIHAS AORUS <mihas.aorus@gmail.com>
2025-10-28 11:42:39 +0800

    Create - Livewire Basic Structure:- - User. - Kategori. - Barang.

--- resources/views/superadmin/user/index.blade.php
+++ resources/views/superadmin/user/index.blade.php
@@ -1,1 +1,8 @@
-@livewire('superadmin.user.index')
+@extends('layouts.app')
+
+@section('title', 'Data User')
+@section('menuSuperadminUser', 'active')
+
+@section('content')
+    @livewire('superadmin.user.index')
+@endsection
