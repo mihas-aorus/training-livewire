b05c12dbacad9714c63f77cb223ac5022d67b267
MIHAS AORUS <mihas.aorus@gmail.com>
2025-10-28 15:08:04 +0800

    Add - Display User.

--- app/Livewire/Superadmin/User/Index.php
+++ app/Livewire/Superadmin/User/Index.php
@@ -2,12 +2,24 @@
 
 namespace App\Livewire\Superadmin\User;
 
+use App\Models\User;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Index extends Component
 {
+    use WithPagination;
+    protected $paginationTheme = 'bootstrap';
+    public $paginate = '10';
+    public $search = '';
     public function render()
     {
-        return view('livewire.superadmin.user.index');
+        $data = array(
+            'user' => User::where('nama', 'like', '%'.$this->search.'%')
+            ->orWhere('email', 'like', '%'.$this->search.'%')
+            ->orderBy('role', 'asc')->paginate($this->paginate),
+        );
+
+        return view('livewire.superadmin.user.index', $data);
     }
 }
--- resources/views/livewire/superadmin/user/index.blade.php
+++ resources/views/livewire/superadmin/user/index.blade.php
@@ -6,26 +6,26 @@
         <section class="content-header">
         <div class="container-fluid">
             <div class="row mb-2">
-            <div class="col-sm-6">
-                <h1>
-                    <i class="fas fa-user mr-2"></i>
-                    @yield('title')
-                </h1>
-            </div>
-            <div class="col-sm-6">
-                <ol class="breadcrumb float-sm-right">
-                    <li class="breadcrumb-item">
-                        <a href="#">
-                            <i class="fas fa-home mr-1"></i>
-                            Dashboard
-                        </a>
-                    </li>
-                    <li class="breadcrumb-item active">
-                        <i class="fas fa-user mr-1"></i>
+                <div class="col-sm-6">
+                    <h1>
+                        <i class="fas fa-user mr-2"></i>
                         @yield('title')
-                    </li>
-                </ol>
-            </div>
+                    </h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item">
+                            <a href="#">
+                                <i class="fas fa-home mr-1"></i>
+                                Dashboard
+                            </a>
+                        </li>
+                        <li class="breadcrumb-item active">
+                            <i class="fas fa-user mr-1"></i>
+                            @yield('title')
+                        </li>
+                    </ol>
+                </div>
             </div>
         </div><!-- /.container-fluid -->
         </section>
@@ -63,7 +63,60 @@
             </div>
 
             <div class="card-body">
-                Data User
+                <div class="mb-3 d-flex justify-content-between">
+                    <div class="col-2">
+                        <select wire:model.live="paginate" class="form-control">
+                            <option value="10">10</option>
+                            <option value="25">25</option>
+                            <option value="50">50</option>
+                            <option value="100">100</option>
+                        </select>
+                    </div>
+                    <div class="col-6">
+                        <input wire:model.live="search" type="text" placeholder="Carian..." class="form-control">
+                    </div>
+                </div>
+                <div class="table-responsive">
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Nama</th>
+                                <th>Email</th>
+                                <th>Role</th>
+                                <th><i class="fas fa-cog"></i></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($user as $item)
+                            <tr>
+                                <td>{{ $loop->iteration }}.</td>
+                                <td>{{ $item->nama }}</td>
+                                <td>{{ $item->email }}</td>
+                                @if ($item->role == 'superadmin')
+                                    <td><span class="badge badge-info">{{ $item->role }}</span></td>
+                                @elseif ($item->role == 'admin')
+                                    <td><span class="badge badge-primary">{{ $item->role }}</span></td>
+                                @else
+                                    <td><span class="badge badge-light  ">{{ $item->role }}</span></td>
+                                @endif
+                                <td>
+                                    <button class="btn btn-sm btn-info">
+                                        <i class="fas fa-eye"></i>
+                                    </button>
+                                    <button class="btn btn-sm btn-warning">
+                                        <i class="fas fa-edit"></i>
+                                    </button>
+                                    <button class="btn btn-sm btn-danger">
+                                        <i class="fas fa-trash"></i>
+                                    </button>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                    {{ $user->links() }}
+                </div>
             </div>
             <!-- /.card-body -->
         </div>
