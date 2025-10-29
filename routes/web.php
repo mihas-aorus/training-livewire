b12a2272096a6dfc879af646ae121b92457d3520
MIHAS AORUS <mihas.aorus@gmail.com>
2025-10-29 10:27:52 +0800

    Update - web route.

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::view('superadmin/user/index', 'superadmin.user.index')->name('superadmin.user.index');
+Route::view('superadmin/user', 'superadmin.user.index')->name('superadmin.user.index');
 Route::view('superadmin/kategori/index', 'superadmin.kategori.index')->name('superadmin.kategori.index');
 Route::view('superadmin/barang/index', 'superadmin.barang.index')->name('superadmin.barang.index');
